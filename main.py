ac118957c09fe8a464b5303536803650b5a5fd58
Mikhail Davydov <mdavydov83@gmail.com>
2022-10-31 23:33:04 +0300

    fix: no more crash with bad data from Excel

--- main.py
+++ main.py
@@ -16,31 +16,40 @@ def POINT(x, y, z):
 def get_total_data(excel_file):
     wb = xw.Book(excel_file)
     sheet = wb.sheets['AutoCAD']
-    total_data = [sheet.range('A1').value,              # get Panel Name
-                  round(sheet.range('B2').value, 2),    # get Py
-                  round(sheet.range('B3').value, 2),    # get Kc
-                  round(sheet.range('B4').value, 2),    # get Pp
-                  round(sheet.range('B5').value, 2),    # get cos f
-                  round(sheet.range('B6').value, 2),    # get Ip
-                  round(sheet.range('B7').value, 2)     # get Isc(3)
+    total_data = [check_and_round('A1'),            # get Panel Name
+                  check_and_round('B2'),            # get Py
+                  check_and_round('B3'),            # get Kc
+                  check_and_round('B4'),            # get Pp
+                  check_and_round('B5'),            # get cos f
+                  check_and_round('B6'),            # get Ip
+                  check_and_round('B7')             # get Isc(3)
                   ]
     return total_data
+
+
+def check_and_round(cell):
+    if sheet.range(cell).value is None:
+        return ''
+    elif type(sheet.range(cell).value) is float:
+        return round(sheet.range(cell).value, 2)
+    else:
+        return sheet.range(cell).value
 
 
 # * read and store balance data of panel from excel sheet named 'AutoCAD'
 def get_balance_data(excel_file):
     wb = xw.Book(excel_file)
     sheet = wb.sheets['AutoCAD']
-    balance_data = [sheet.range('A9').value,               # is balance nedd to be inserted (Yes/No)
-                    round(sheet.range('B10').value, 1),    # get L1_Pp
-                    round(sheet.range('B11').value, 1),    # get L2_Pp
-                    round(sheet.range('B12').value, 1),    # get L3_Pp
-                    round(sheet.range('C10').value, 1),    # get L1_Ip
-                    round(sheet.range('C11').value, 1),    # get L2_Ip
-                    round(sheet.range('C12').value, 1),    # get L3_Ip
-                    round(sheet.range('D10').value),       # get L1_KNF
-                    round(sheet.range('D11').value),       # get L2_KNF
-                    round(sheet.range('D12').value)        # get L3_KNF
+    balance_data = [check_and_round('A9'),          # is balance nedd to be inserted (Yes/No)
+                    check_and_round('B10'),         # get L1_Pp
+                    check_and_round('B11'),         # get L2_Pp
+                    check_and_round('B12'),         # get L3_Pp
+                    check_and_round('C10'),         # get L1_Ip
+                    check_and_round('C11'),         # get L2_Ip
+                    check_and_round('C12'),         # get L3_Ip
+                    check_and_round('D10'),         # get L1_KNF
+                    check_and_round('D11'),         # get L2_KNF
+                    check_and_round('D12')          # get L3_KNF
                     ]
     return balance_data
 
@@ -48,21 +57,21 @@ def get_balance_data(excel_file):
 def get_incomer_data(excel_file):
     wb = xw.Book(excel_file)
     sheet = wb.sheets['AutoCAD']
-    incomer_data = [sheet.range('A21').value,
-                    sheet.range('A22').value,
-                    sheet.range('A23').value,
-                    sheet.range('A24').value,
-                    sheet.range('C21').value,
-                    sheet.range('C22').value,
-                    sheet.range('C23').value,
-                    sheet.range('C24').value,
-                    sheet.range('F21').value,
-                    sheet.range('F22').value,
-                    sheet.range('B21').value,
-                    sheet.range('B22').value,
-                    sheet.range('B23').value,
-                    sheet.range('B24').value,
-                    sheet.range('D1').value
+    incomer_data = [check_and_round('A21'),
+                    check_and_round('A22'),
+                    check_and_round('A23'),
+                    check_and_round('A24'),
+                    check_and_round('C21'),
+                    check_and_round('C22'),
+                    check_and_round('C23'),
+                    check_and_round('C24'),
+                    check_and_round('F21'),
+                    check_and_round('F22'),
+                    check_and_round('B21'),
+                    check_and_round('B22'),
+                    check_and_round('B23'),
+                    check_and_round('B24'),
+                    check_and_round('D1')
                     ]
     return incomer_data
 
@@ -118,7 +127,9 @@ def round_data(source_range):  # * rounding floats in data from 'AutoCAD' sheet 
 def column_round(column):  # * part of previus function
     rounded_column = []
     for item in column:
-        if type(item) == float:
+        if item is None:
+            rounded_column.append('')
+        elif type(item) is float:
             if modf(item)[0] == 0:
                 rounded_column.append(round(item))
             else:
